bff62a61257c62fed21aa80acc5a4d71bca1bddf
Ashwithagoud78 <bathiniashwithagoud@gmail.com>
2026-02-21 11:16:47 +0530

    Index2

---
+++ index2.js
@@ -0,0 +1,17 @@
+import express from 'express'
+const app = express()
+app.listen(8080, () => {
+    console.log('Server is running on port 8080')
+})
+
+app.get('/', (req, res) => {
+    console.log(req.url);
+    console.log(req.method);
+    res.send('Hello World')
+});
+app.post('/', (req, res) => {
+    console.log(req.url);
+    console.log(req.method);
+    res.send('This is the home page')
+});
+
