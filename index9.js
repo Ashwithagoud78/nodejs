4cae125a5fc5302c0327c2043d2b0a0ffb932fce
Ashwithagoud78 <bathiniashwithagoud@gmail.com>
2026-02-21 15:49:37 +0530

    Index9

---
+++ index9.js
@@ -0,0 +1,25 @@
+import express from "express";
+const app = express();
+app.listen(8080);
+app.use(express.json());
+let users = [];
+app.get("/", (req, res) => {
+    res.send("Hello World");
+});
+app.post("/signup", (req, res) => {
+    const user = req.body;
+    users.push(user);
+    res.json(users);
+});
+app.post("/login", (req, res) => {
+    const { email, password } = req.body;
+    const found = users.find((user) => user.email === email && user.password === password);
+    if (found) {
+        res.send("Login successful");
+    } else {
+        res.send("Invalid credentials");
+    }
+});
+app.get("/users", (req, res) => {
+    res.json(users);
+});
