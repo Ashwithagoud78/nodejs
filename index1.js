8b1d06e971c3c1c9aa403c65c7f6587705fd1bbd
Ashwithagoud78 <bathiniashwithagoud@gmail.com>
2026-02-21 11:03:12 +0530

    Hello

--- index1.js
+++ index1.js
@@ -1,1 +1,13 @@
-console.log("Hello, World!");
+import express from 'express'
+const app = express()
+app.listen(8080, () => {
+    console.log('Server is running on port 8080')
+})
+
+app.get('/', (req, res) => {
+    res.send('Hello World')
+})
+
+app.get('/home', (req, res) => {
+    res.send('This is the home page')
+})
